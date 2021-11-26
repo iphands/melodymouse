625b6cf80eee1aa6e85e6696de59ee00082e0dbf
Ian Page Hands <iphands@gmail.com>
2021-11-26 10:23:14 -0600

    Change to deterministic

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
+import itertools
 import random
 import hashlib
+import sys
+import threading
 from datetime import datetime
+from collections import deque
+
+FINISHED = False
+
+THREADS = 30
 
 BU = 1
 BD = -1
@@ -26,8 +34,8 @@ cards = [
 ]
 
 # cards = [
-#     [ BD, BU, BD, RD ],
-#     [ BD, BU, BD, PU ],
+#     [ BD, BU, BD, BU ],
+#     [ BD, BU, BD, BU ],
 #     [ BD, BU, BD, BU ],
 #     [ BD, BU, BD, BU ],
 #     [ BD, BU, BD, BU ],
@@ -37,8 +45,10 @@ cards = [
 #     [ BD, BU, BD, BU ],
 # ]
 
-def rotate(c):
-    return [ c[3], c[0], c[1], c[2] ]
+def rotate(c, n):
+    tmp = deque(c)
+    tmp.rotate(n)
+    return list(tmp)
 
 def get_rand():
     for card_i, c in enumerate(cards):
@@ -47,10 +57,9 @@ def get_rand():
         for i in range(0, n):
             cards[card_i] = rotate([c[0], c[1], c[2], c[3]])
             # print(cards[card_i])
-
     random.shuffle(cards)
 
-def validate():
+def validate(cards):
     # Top row
     if cards[0][1] != cards[1][3] * -1:
         return False
@@ -90,75 +99,124 @@ def get_card_ascii(num):
         return " {}".format(num)
     return num
 
-def print_row(num):
+def print_row(c, num):
     print("""
    {}       {}       {}
 {} + {}  {} + {}  {} + {}
    {}       {}       {}
     """.format(
-        get_card_ascii(cards[num + 0][0]),
-        get_card_ascii(cards[num + 1][0]),
-        get_card_ascii(cards[num + 2][0]),
+        get_card_ascii(c[num + 0][0]),
+        get_card_ascii(c[num + 1][0]),
+        get_card_ascii(c[num + 2][0]),
 
-        get_card_ascii(cards[num + 0][3]),
-        get_card_ascii(cards[num + 0][1]),
-        get_card_ascii(cards[num + 1][3]),
-        get_card_ascii(cards[num + 1][1]),
-        get_card_ascii(cards[num + 2][3]),
-        get_card_ascii(cards[num + 2][1]),
+        get_card_ascii(c[num + 0][3]),
+        get_card_ascii(c[num + 0][1]),
+        get_card_ascii(c[num + 1][3]),
+        get_card_ascii(c[num + 1][1]),
+        get_card_ascii(c[num + 2][3]),
+        get_card_ascii(c[num + 2][1]),
 
-        get_card_ascii(cards[num + 0][2]),
-        get_card_ascii(cards[num + 1][2]),
-        get_card_ascii(cards[num + 2][2]),
+        get_card_ascii(c[num + 0][2]),
+        get_card_ascii(c[num + 1][2]),
+        get_card_ascii(c[num + 2][2]),
     ))
 
-def print_cards():
-    print_row(0)
-    print_row(3)
-    print_row(6)
+def print_cards(c):
+    print_row(c, 0)
+    print_row(c, 3)
+    print_row(c, 6)
 
-def main():
+def do_test(i, rot_matrix, pcards, results, j):
+    print(i)
+    # if i == SAMPLE:
+    #     i = 0
+    #     end = datetime.now()
+    #     ms = (end - start).total_seconds()
+    #     print("tps: {}".format(SAMPLE / ms))
+    #     start = datetime.now()
+
+    i += 1
+    # get_rand()
+
+    # sum_str = hashlib.md5(str(cards).encode('utf-8')).hexdigest()
+    # if sum_str in sums:
+    #     print("MISS")
+    #     continue
+    # sums.append(sum_str)
+
+    # print("{}: {}".format(len(sums), sum_str))
+
+    # print("------------------")
+    # print(cards)
+
+    for m in rot_matrix:
+        tmp = rotate_all(m, list(pcards))
+        if validate(tmp):
+            # print("FOUND in {} tries".format(i))
+            # print(tmp)
+            results[j] = tmp
+            return
+
+def main(rot_matrix):
     i = 0
+    j = 0
+    threads = []
     start = datetime.now()
     end   = datetime.now()
 
     SAMPLE = 100000
+    results = [False] * THREADS
 
-    while(True):
-        # if i == SAMPLE:
-        #     i = 0
-        #     end = datetime.now()
-        #     ms = (end - start).total_seconds()
-        #     print("tps: {}".format(SAMPLE / ms))
-        #     start = datetime.now()
+    for pcards in itertools.permutations(cards):
+        if j == THREADS:
+            for t in threads:
+                t.join()
+                for r in results:
+                    if r:
+                        print("Found!")
+                        print(r)
+                        sys.exit(0)
+            j = 0
+            results = [False] * THREADS
 
+
+        t = threading.Thread(target=do_test, args=(i, rot_matrix, pcards, results, j))
+        threads.append(t)
+        t.start()
+        # do_test(i, rot_matrix, pcards)
         i += 1
-        get_rand()
+        j += 1
 
-        # sum_str = hashlib.md5(str(cards).encode('utf-8')).hexdigest()
-        # if sum_str in sums:
-        #     print("MISS")
-        #     continue
-        # sums.append(sum_str)
+def get_rot_matrix():
+    ret = []
+    a = [ 0, 0, 0, 0, 0, 0, 0, 0, 0 ]
+    # a = [ 0, 0, 0 ]
+    i = 0
 
-        # print("{}: {}".format(len(sums), sum_str))
+    try:
+        while True:
+            ret.append(a[:])
+            a[i] += 1
+            while True:
+                if a[i] == 4:
+                    a[i] = 0
+                    i += 1
+                    a[i] += 1
+                    continue
+                i = 0
+                break
+    except:
+        pass
+    return ret
 
-        # print("------------------")
-        # print(cards)
-        # print_cards()
-        if validate():
-            # print_cards()
-            print("FOUND in {} tries".format(i))
-            print(cards)
-            print_cards()
-            break
+def rotate_all(m, cs):
+    for i, c in enumerate(cs):
+        cs[i] = rotate(c, m[i])
+    return cs
 
 def test():
-    for i in range(0, 10):
-        print("-----------")
-        print(cards[0])
-        # cards[0] = rotate(cards[0])
-        get_rand()
+    a = [ 1, 2, 3 ]
+    for t in itertools.permutations(a):
+        print(t)
 
-# test()
-main()
+main(get_rot_matrix())
